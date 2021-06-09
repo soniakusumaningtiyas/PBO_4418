bdabcecd382d60224d56a0bc0ff4a389f05cca4f
soniakusumaningtiyas <80149169+soniakusumaningtiyas@users.noreply.github.com>
2021-06-09 19:44:10 +0700

    Add files via upload

--- pertemuan12/src/pertemuan12/Koneksiku.java
+++ pertemuan12/src/pertemuan12/Koneksiku.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+
 /**
  *
  * @author ACER
@@ -18,15 +19,14 @@ public class Koneksiku {
     /**
      * @param args the command line arguments
      */
-    private static Connection MySQLConfig;
+   private static Connection MySQLConfig;
     public static Connection configDB() throws SQLException{
         try{
             String url="jdbc:mysql://localhost:3306/db_dutacoding";
             String user = "root";
             String pass = "";
             
-            DriverManager registerDriver;
-            registerDriver = DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             MySQLConfig = DriverManager.getConnection(url,user,pass);
         }catch (SQLException e){
             System.out.println("Koneksi Ke DataBase GAGAL "+e.getMessage()); 

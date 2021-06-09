6a970a3fa1473a0bacabce713a6590a7ee5903bb
soniakusumaningtiyas <80149169+soniakusumaningtiyas@users.noreply.github.com>
2021-06-09 18:42:41 +0700

    Add files via upload

---
+++ pertemuan12/src/pertemuan12/Koneksiku.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan12;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+/**
+ *
+ * @author ACER
+ */
+public class Koneksiku {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Connection MySQLConfig;
+    public static Connection configDB() throws SQLException{
+        try{
+            String url="jdbc:mysql://localhost:3306/db_dutacoding";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager registerDriver;
+            registerDriver = DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url,user,pass);
+        }catch (SQLException e){
+            System.out.println("Koneksi Ke DataBase GAGAL "+e.getMessage()); 
+        }
+        return MySQLConfig;
+    }
+}
